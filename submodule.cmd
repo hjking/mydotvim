fc321600d978bd68334352cc153834f8e9aabce0
kinghom <bestkindy@gmail.com>
2012-04-27 19:11:23 +0800

    initial

---
+++ submodule.cmd
@@ -0,0 +1,15 @@
+git submodule add https://github.com/gmarik/vundle.git bundle/vundle
+git submodule add https://github.com/tpope/vim-fugitive.git bundle/fugitive
+git submodule add https://github.com/vim-scripts/closetag.vim.git bundle/closetag.vim
+git submodule add https://github.com/Rykka/ColorV.git bundle/ColorV
+git submodule add https://github.com/vim-scripts/cscope_win.git bundle/cscope_win
+git submodule add https://github.com/vim-scripts/cucumber.zip.git bundle/cucumber.zip
+git submodule add https://github.com/vim-scripts/Decho.git bundle/Decho
+git submodule add https://github.com/vim-scripts/DrawIt.git bundle/DrawIt
+git submodule add https://github.com/vim-scripts/file-line.git bundle/file-line
+git submodule add https://github.com/vim-scripts/FuzzyFinder.git bundle/FuzzyFinder
+git submodule add https://github.com/vim-scripts/git.zip.git bundle/git.zip
+git submodule add https://github.com/vim-scripts/Align.git bundle/Align
+git submodule add https://github.com/vim-scripts/Markdown.git bundle/Markdown
+git submodule add https://github.com/vim-scripts/L9.git bundle/L9
+git submodule add https://github.com/tomasr/molokai.git bundle/molokai
